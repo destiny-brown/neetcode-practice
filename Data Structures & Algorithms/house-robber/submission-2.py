0807143934202ae4c11de1db4545c3760e173371
Destiny Brown <119943684+destiny-brown@users.noreply.github.com>
2026-08-18 12:30:30 +0800

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        max_money = {}
+        max_money[0] = nums[0]
+        max_money[1] = max(max_money[0], nums[1])
+
+        for i in range(2, len(nums)):
+            max_money[i] = max(max_money[i-1], nums[i]+max_money[i-2])
+
+        return max_money[len(nums)-1]
